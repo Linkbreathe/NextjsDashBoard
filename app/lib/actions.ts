4c9a2a7febbf520dde7ceac0cccb6066c2c23965
Link <61148194+Linkbreathe@users.noreply.github.com>
2024-07-04 12:17:07 +0800

    Chapter 15 Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -28,6 +30,7 @@ export type State = {
 };
 
 // Use Zod to create the expected types
+// Omit = Exclude specified properties or fields
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 // Use Zod to update the expected types
@@ -50,7 +53,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     const customerId = formData.get('customerId') as string;
     const amount = parseFloat(formData.get('amount') as string); // Ensure amount is converted to number
     const status = formData.get('status') as string;
-    
+
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
     try {
@@ -79,7 +82,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(prevState: State, id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -92,9 +95,7 @@ export async function updateInvoice(prevState: State, id: string, formData: Form
         }
     }
 
-    const customerId = formData.get('customerId') as string;
-    const amount = parseFloat(formData.get('amount') as string); // Ensure amount is converted to number
-    const status = formData.get('status') as string;
+    const { customerId, amount, status } = validatedFields.data;
 
     const amountInCents = amount * 100;
     try {
@@ -120,5 +121,24 @@ export async function deleteInvoice(id: string) {
         return { message: 'Deleted Invoice.' };
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
     }
 }
